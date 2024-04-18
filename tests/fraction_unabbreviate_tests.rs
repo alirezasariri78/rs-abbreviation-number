f1de292f47d9cf4a6ddb1eda9ce13a93cd6086af
alireza_sariri <alirezasariri78@gmail.com>
2024-04-18 21:35:25 +0330

    implement tests for unabbr fraction

--- tests/fraction_unabbreviate_tests.rs
+++ tests/fraction_unabbreviate_tests.rs
@@ -50,4 +50,25 @@ mod tests {
             "100q".unabbreviate_number()
         );
     }
+
+    #[test]
+    fn deep_random_number_test() {
+        assert_eq!(0.000_000_000_193, "193p".unabbreviate_number());
+        assert_eq!(0.000_000_000_000_102, "102f".unabbreviate_number());
+        assert_eq!(0.000_000_000_000_000_100, "100a".unabbreviate_number());
+        assert_eq!(0.000_000_000_000_000_000_001, "1z".unabbreviate_number());
+        assert_eq!(0.000_000_000_000_000_000_111, "111z".unabbreviate_number());
+    }
+
+    #[test]
+    fn deep_negative_random_number_test() {
+        assert_eq!(-0.000_000_000_193, "-193p".unabbreviate_number());
+        assert_eq!(-0.000_000_000_000_102, "-102f".unabbreviate_number());
+        assert_eq!(-0.000_000_000_000_000_100, "-100a".unabbreviate_number());
+        assert_eq!(-0.000_000_000_000_000_000_001, "-1z".unabbreviate_number());
+        assert_eq!(
+            -0.000_000_000_000_000_000_111,
+            "-111z".unabbreviate_number()
+        );
+    }
 }
