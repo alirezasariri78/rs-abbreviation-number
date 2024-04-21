5bfa1f01bfd0190b552d6d1cc4640bf60e78c196
Ali Ghahremani <alighahremani1377@gmail.com>
2024-04-21 19:44:29 +0330

    feat: separator option for abbreviate

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,14 @@ pub trait NumericAbbreviate {
     /// ```
     /// use rs_abbreviation_number::*;
     /// let number=110_000;
-    /// let result=number.abbreviate_number();
+    /// let result=number.abbreviate_number(&Default::default());
     /// assert_eq!("110K",result);
     ///
     /// let number=0.000_1;
-    /// let result=number.abbreviate_number();
+    /// let result=number.abbreviate_number(&Default::default());
     /// assert_eq!("100μ",result);
     /// ```
-    fn abbreviate_number(&self) -> String;
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String;
 }
 
 pub trait NumericUnAbbreviate {
@@ -35,80 +35,80 @@ pub trait NumericUnAbbreviate {
 }
 
 impl NumericAbbreviate for f64 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self,options)
     }
 }
 
 impl NumericAbbreviate for f32 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for i128 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for i64 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for i32 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for i16 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for i8 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for usize {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for u128 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for u64 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for u32 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for u16 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
 impl NumericAbbreviate for u8 {
-    fn abbreviate_number(&self) -> String {
-        handle_abbreviation(*self as f64)
+    fn abbreviate_number(&self, options: &AbbreviationOptions) -> String {
+        handle_abbreviation(*self as f64,options)
     }
 }
 
@@ -124,10 +124,20 @@ impl NumericUnAbbreviate for &str {
     }
 }
 
-fn handle_abbreviation(number: f64) -> String {
+fn handle_abbreviation(number: f64, options: &AbbreviationOptions) -> String {
     if number.abs() >= 1.0 || number == 0.0 {
-        abbreviate_number(number)
+        abbreviate_number(number,options)
     } else {
-        abbreviate_fraction_number(number)
+        abbreviate_fraction_number(number,options)
+    }
+}
+
+pub struct AbbreviationOptions{
+    pub separator: String
+}
+
+impl Default for AbbreviationOptions {
+    fn default() -> Self {
+        Self { separator: "".to_string() }
     }
 }
--- tests/fraction_abbreviate_tests.rs
+++ tests/fraction_abbreviate_tests.rs
@@ -4,25 +4,25 @@ mod tests {
 
     #[test]
     fn small_number_test() {
-        assert_eq!("10n", (0.000_000_010).abbreviate_number());
-        assert_eq!("643n", (0.000_000_643).abbreviate_number());
+        assert_eq!("10n", (0.000_000_010).abbreviate_number(&Default::default()));
+        assert_eq!("643n", (0.000_000_643).abbreviate_number(&Default::default()));
     }
     #[test]
     fn small_negative_number_test() {
-        assert_eq!("-10n", (-0.000_000_010).abbreviate_number());
-        assert_eq!("-643n", (-0.000_000_643).abbreviate_number());
+        assert_eq!("-10n", (-0.000_000_010).abbreviate_number(&Default::default()));
+        assert_eq!("-643n", (-0.000_000_643).abbreviate_number(&Default::default()));
     }
 
     #[test]
     fn deep_number_test() {
         assert_eq!(
             "100q",
-            (0.000_000_000_000_000_000_000_000_000_1).abbreviate_number()
+            (0.000_000_000_000_000_000_000_000_000_1).abbreviate_number(&Default::default())
         );
 
         assert_eq!(
             "0",
-            0.000_000_000_000_000_000_000_000_000_000_1.abbreviate_number()
+            0.000_000_000_000_000_000_000_000_000_000_1.abbreviate_number(&Default::default())
         );
     }
 
@@ -30,12 +30,12 @@ mod tests {
     fn deep_negative_number_test() {
         assert_eq!(
             "-100q",
-            (-0.000_000_000_000_000_000_000_000_000_1).abbreviate_number()
+            (-0.000_000_000_000_000_000_000_000_000_1).abbreviate_number(&Default::default())
         );
 
         assert_eq!(
             "0",
-            (-0.000_000_000_000_000_000_000_000_000_000_1).abbreviate_number()
+            (-0.000_000_000_000_000_000_000_000_000_000_1).abbreviate_number(&Default::default())
         );
     }
 }
